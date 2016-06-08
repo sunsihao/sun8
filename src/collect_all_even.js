69221b2a2151d47f0a6074e2ca901a46c535f48a
52UUD <varme@139.com>
2016-06-08 21:37:19 +0800

    first

---
+++ src/collect_all_even.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function collect_all_even(collection) {
+	var arr = [];
+	for(var i = 0; i < collection.length; i++){
+		if (collection[i] %2 == 0) {
+			arr.push(collection[i]);
+			continue;
+		}
+	}
+	return arr;
+}
+
+module.exports = collect_all_even;
